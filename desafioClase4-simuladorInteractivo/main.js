72c39ce10a19516d3fe690e04cc4963fc5eb7f50
patoviola <patricioviola26@gmail.com>
2022-08-23 20:12:01 -0300

    desafio clase 4

---
+++ desafioClase4-simuladorInteractivo/main.js
@@ -0,0 +1,28 @@
+const direccion1 = prompt("Ingrese la direccion de partida")
+const direccion2 = prompt("Ingresa direccion de destino")
+const cantidadDeKM = prompt("Indique cantidad de KM")
+let valorKM = 80
+
+function valorDelViaje(){
+    const valorFinal = cantidadDeKM * valorKM;
+    console.log("El valor del viaje es:", "$" + valorFinal + ".-")
+}
+
+function direccionDeInicio(){
+    console.log("Direccion de partida:", direccion1)
+}
+
+function direccionDeFin(){
+    console.log("Direccion de llegada:", direccion2)
+}
+
+
+
+direccionDeInicio()
+direccionDeFin()
+valorDelViaje()
+
+
+
+
+
